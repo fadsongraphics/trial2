ecd44d0ee99e91239fdab7d170a2d95baa19e619
Stephen Ogunmuyiwa. IDM. India <75666449+fadsongraphics@users.noreply.github.com>
2021-06-14 14:59:48 +0100

    Update loop test.py

--- loop test.py
+++ loop test.py
@@ -481,13 +481,13 @@ def intent2action(intent):
 
 def speakword(text):
 
-  # engine = pyttsx3.init()
-  # voices = engine.getProperty('voices')
-  # engine.setProperty('voice', voices[1].id)
-  # engine.setProperty('rate', 140)
-  # engine.say(text)
-  # engine.runAndWait()
-  print(text)
+  if text !=='':
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('rate', 140)
+    engine.say(text)
+    engine.runAndWait()
     
     
 q = queue.Queue()
